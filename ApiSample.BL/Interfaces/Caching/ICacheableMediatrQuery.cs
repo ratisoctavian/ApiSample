7ccab2f9844a2b68b28b7d39c0ffbb1eb06c8764
astig basca <astig.basca@gmail.com>
2022-10-03 17:52:37 +0300

    Added caching behavior. Error in caching behavior code

---
+++ ApiSample.BL/Interfaces/Caching/ICacheableMediatrQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSample.BL.Interfaces.Caching
+{
+    public interface ICacheableMediatrQuery
+    {
+        bool BypassCache { get; }
+        string CacheKey { get; }
+        TimeSpan? SlidingExpiration { get; }
+    }
+}
